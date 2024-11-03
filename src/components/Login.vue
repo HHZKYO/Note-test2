d7e4ec5b3f06cee5996d978127a12703b4783623
HHZKYO <1692189552@qq.com>
2024-11-03 19:33:17 +0800

    login组件功能实现

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -5,17 +5,19 @@
         <div class="modal-container">
           <div class="main"></div>
           <div class="form">
-            <h3>创建账户</h3>
-            <div v-show="true" class="register">
-              <input type="text" placeholder="用户名">
-              <input type="password" placeholder="密码">
-              <div class="button">创建账号</div>
+            <h3 @click="showRegister">创建账户</h3>
+            <div v-show="isShowRegister" class="register">
+              <input type="text" v-model="register.username" placeholder="用户名">
+              <input type="password" v-model="register.password" placeholder="密码">
+              <p v-bind:class="{error: register.isError}">{{ register.notice }}</p>
+              <div class="button" @click="onRegister">创建账号</div>
             </div>
-            <h3>登录</h3>
-            <div v-show="false" class="login">
-              <input type="text" placeholder="输入用户名">
-              <input type="password" placeholder="密码">
-              <div class="button">登录</div>
+            <h3 @click="showLogin">登录</h3>
+            <div v-show="isShowLogin" class="login">
+              <input type="text" v-model="login.username" placeholder="输入用户名">
+              <input type="password" v-model="login.password" placeholder="密码">
+              <p v-bind:class="{error: login.isError}">{{ login.notice }}</p>
+              <div class="button" @click="onLogin">登录</div>
             </div>
           </div>
         </div>
@@ -23,6 +25,69 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      isShowLogin: true,
+      isShowRegister: false,
+      login: {
+        username: '',
+        password: '',
+        notice: '输入用户名和密码',
+        isError: false
+      },
+      register: {
+        username: '',
+        password: '',
+        notice: '创建账号后，请记住用户名和密码',
+        isError: false
+      }
+    }
+  },
+  methods: {
+    showLogin() {
+      this.isShowLogin = true
+      this.isShowRegister = false
+    },
+    showRegister() {
+      this.isShowLogin = false
+      this.isShowRegister = true
+    },
+    onRegister() {
+      if(!/^[\w\u4e00-\u9fa5]{3,15}$/.test(this.register.username)) {
+        this.register.isError = true
+        this.register.notice = '用户名为3~15个字符，仅限于字母数字下划线中文'
+        return
+      }
+      if(!/^.{6,16}$/.test(this.register.password)) {
+        this.register.isError = true
+        this.register.notice = '密码长度为6~16个字符'
+        return
+      }
+      this.register.isError = false
+      this.register.notice = ''
+      console.log(`start register..., username: ${this.register.username}, password: ${this.register.password}`)
+    },
+    onLogin() {
+      if(!/^[\w\u4e00-\u9fa5]{3,15}$/.test(this.login.username)) {
+        this.login.isError = true
+        this.login.notice = '用户名为3~15个字符，仅限于字母数字下划线中文'
+        return
+      }
+      if(!/^.{6,16}$/.test(this.login.password)) {
+        this.login.isError = true
+        this.login.notice = '密码长度为6~16个字符'
+        return
+      }
+      this.login.isError = false
+      this.login.notice = ''
+      console.log(`start login..., username: ${this.login.username}, password: ${this.login.password}`)
+    }
+  }
+}
+</script>
 
 <style lang="less">
 .modal-mask {
@@ -55,9 +120,10 @@
 
   .main {
     flex: 1;
-    background: #36bc64 url(../assets/note.jpg) center center no-repeat;
+    background: #36bc64 url(//cloud.hunger-valley.com/17-12-13/38476998.jpg-middle) center center no-repeat;
     background-size: contain;
   }
+
   .form {
     width: 270px;
     border-left: 1px solid #ccc;
@@ -69,7 +135,7 @@
       border-top: 1px solid #eee;
       cursor: pointer;
 
-      &:nth-of-type(2){
+      &:nth-of-type(2) {
         border-bottom: 1px solid #eee;
       }
     }
@@ -86,7 +152,8 @@
       cursor: pointer;
     }
 
-    .login,.register {
+    .login,
+    .register {
       padding: 10px 20px;
       border-top: 1px solid #eee;
 
@@ -102,6 +169,7 @@
         font-size: 14px;
         margin-top: 10px;
       }
+
       input:focus {
         border: 3px solid #9bcaf8;
       }
@@ -111,10 +179,12 @@
         margin-top: 10px;
         color: #444;
       }
+
       .error {
         color: red;
       }
     }
+
     .login {
       border-top: 0;
     }
